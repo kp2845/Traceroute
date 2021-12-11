9000da4bbece8075f45197c5738ef4ff657a6938
kp2845 <90535253+kp2845@users.noreply.github.com>
2021-12-11 18:17:51 -0500

    Showed print out of correct input per line. WIl ot go into a string?

--- solution.py
+++ solution.py
@@ -43,13 +43,13 @@ def build_packet():
     # packet to be sent was made, secondly the checksum was appended to the header and
     # then finally the complete packet was sent to the destination.
     myID = os.getpid() & 0xFFFF  # Return the current process id
-
+    #Need PID to populate the struct
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, 0, myID, 1)
     data = struct.pack("d", time.time())
     # Calculate the checksum on the data and the dummy header.
     myChecksum = checksum(header + data)
 
-    # Make the header in a similar way to the ping exercise.
+    # Make the header in a similar way to the ping exercise. Copied from ping lab.
     # Append checksum to the header.
     if sys.platform == 'darwin':
         # Convert 16-bit integers from host to network  byte order
@@ -68,8 +68,6 @@ def build_packet():
 
 def get_route(hostname):
     destAddr = gethostbyname(hostname)
-    #print("Get traceroute for {} ({}): {} Max Hops, {} Max Retries".format(hostname, destAddr, MAX_HOPS, TRIES))
-    print()
     timeLeft = TIMEOUT
     tracelist1 = [] #This is your list to use when iterating through each trace
     tracelist2 = [] #This is your list to contain all traces
@@ -92,7 +90,7 @@ def get_route(hostname):
             try:
                 d = build_packet()
                 mySocket.sendto(d, (hostname, 0))
-                t= time.time()
+                t = time.time()
                 startedSelect = time.time()
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
@@ -100,7 +98,7 @@ def get_route(hostname):
                     tracelist1.append("* * * Request timed out.")
                     #Fill in start
                     #You should add the list above to your all traces list
-                    tracelist2.append([str(ttl), "*", "Request timed out."])
+                    tracelist2.append([str(ttl), "*", "Request timed out"])
                     #Fill in end
                 recvPacket, addr = mySocket.recvfrom(1024)
                 timeReceived = time.time()
@@ -109,7 +107,7 @@ def get_route(hostname):
                     tracelist1.append("* * * Request timed out.")
                     #Fill in start
                     #You should add the list above to your all traces list
-                    tracelist2.append([str(ttl), "*", "Request timed out."])
+                    tracelist2.append([str(ttl), "*", "Request timed out"])
                     #Fill in end
             except timeout:
                 continue
@@ -121,12 +119,14 @@ def get_route(hostname):
                 #Fill in end
                 try: #try to fetch the hostname
                     #Fill in start
-                    hostname_try = gethostbyname(hostname)
+                    hostname_try = gethostbyaddr(addr[0])
                     #Fill in end
-                except herror:   #if the host does not provide a hostname
+                except herror as e:   #if the host does not provide a hostname
                     #Fill in start
                     hostname_try = 'hostname not returnable'
                     #Fill in end
+                if isinstance(hostname_try, tuple):
+                    hostname_try = hostname_try[0]
 
                 if types == 11:
                     bytes = struct.calcsize("d")
@@ -134,34 +134,38 @@ def get_route(hostname):
                     #Fill in start
                     #You should add your responses to your lists here
                     delay = timeReceived - t
-                    delay_as_string = str(delay)
-                    tracelist2.append([str(ttl), delay_as_string, hostname, hostname_try])
+                    delay = round(delay * 1000)
+                    delay_as_string = str(delay) + "ms"
+                    tracelist2.append([str(ttl), delay_as_string, addr[0], hostname_try])
                     #Fill in end
-                    #print:('{}\t{}\t{}t'.format(str(ttl), delay_as_string, hostname, hostname_try))
+                    print('{}\t{}\t{}\t{}'.format(str(ttl), delay_as_string, addr[0], hostname_try))
                 elif types == 3:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here
                     delay = timeReceived - t
+                    delay = round(delay * 1000)
                     delay_as_string = str(delay) + "ms"
-                    tracelist2.append([str(ttl), delay_as_string, hostname, hostname_try])
+                    tracelist2.append([str(ttl), delay_as_string, addr[0], hostname_try])
                     #Fill in end
-                    #print: ('{}\t{}\t{}t'.format(str(ttl), delay_as_string, hostname, hostname_try))
+                    print('{}\t{}\t{}\t{}'.format(str(ttl), delay_as_string, addr[0], hostname_try))
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     delay = timeReceived - timeSent
+                    delay = round(delay * 1000)
                     delay_as_string = str(delay) + "ms"
-                    tracelist2.append([str(ttl), delay_as_string, hostname, hostname_try])
+                    tracelist2.append([str(ttl), delay_as_string, addr[0], hostname_try])
                     #Fill in end
-                    #print: ('{}\t{}\t{}t'.format(str(ttl), delay_as_string, hostname, hostname_try))
+                    print('{}\t{}\t{}\t{}'.format(str(ttl), delay_as_string, addr[0], hostname_try))
                 else:
                     #Fill in start
                     #If there is an exception/error to your if statements, you should append that to your list here
                     tracelist2.append([str(ttl), '*', 'Request timed out'])
+                    print('{}\t{}\t{}\t{}'.format(str(ttl), '*', 'Request timed out'))
                     #Fill in end
                 break
             finally:
